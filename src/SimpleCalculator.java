6fe92663c3715a7f668405859505882215ebf1dc
angelinevillachica <angeline.villachica@jointheleague.org>
2018-06-30 11:04:32 -0700

    Add files via upload

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -6,10 +6,13 @@ public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-
-		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+String n=JOptionPane.showInputDialog("Give me a number");
+		String nn=JOptionPane.showInputDialog("Give me another number.");
+int num=Integer.parseInt(n);
+int num2=Integer.parseInt(nn);		
+				// 2. Customize pop-up to support add/subtract/multiply/divide operations.
+		int operation = JOptionPane.showOptionDialog(null, "Do you want to add, multiply, divide, or subtract?", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add", "Subtract", "Multiply", "Divide" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
@@ -20,6 +23,6 @@ public class SimpleCalculator {
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
-
+public static void
 	// 4. Create similar methods for subtraction, multiplication and division.
 }
