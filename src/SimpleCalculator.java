081edb08299042f7fd1d72d2c453aafdcfafd114
angelinevillachica <angeline.villachica@jointheleague.org>
2018-07-07 10:59:23 -0700

    Add files via upload

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -4,6 +4,8 @@ import javax.swing.JOptionPane;
 
 public class SimpleCalculator {
 	public static void main(String[] args) {
+for (int i = 0; i < 2; i++) {
+	
 
 		// 1. Get 2 numbers from the user and convert them to integer.
 String n=JOptionPane.showInputDialog("Give me a number");
@@ -17,12 +19,40 @@ int num2=Integer.parseInt(nn);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
 
+if(operation==0) {
+	add(num, num2);
+}
+if(operation==1) {
+	subtract(num, num2);
+}
+if(operation==2) {
+	multiply(num, num2);
+}
+if(operation==3) {
+divide(num, num2);	
+}}
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
+	
 	}
-
+	
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
-public static void
+static void add(int num,int num2){ 
+	JOptionPane.showMessageDialog(null, num+" + "+num2+"="+(num+num2));
+}
 	// 4. Create similar methods for subtraction, multiplication and division.
+static void subtract(int num,int num2){
+	JOptionPane.showMessageDialog(null, num+" - "+num2+"="+(num-num2));
+	}
+
+static void multiply(int num,int num2){
+	JOptionPane.showMessageDialog(null, num+" x "+num2+"="+(num*num2));
+}
+
+static void divide(int num,int num2){   
+	JOptionPane.showMessageDialog(null, num+" / "+num2+"="+(num/num2));
+}
+
+
 }
